7ad3ae0f028b98f8d1d021d5d62d5858c6c77a7c
danny2004de <desaisannidhya43@gmail.com>
2024-12-01 13:03:22 -0600

    better day 1

--- adv01.py
+++ adv01.py
@@ -3,17 +3,26 @@ from collections import Counter
 def part1(filename): 
     f = open(filename, 'r')
     lines = f.read().splitlines()
-    l1 = [int(line.split()[0]) for line in lines]
-    l2 = [int(line.split()[1]) for line in lines]
+    l1, l2 = [], []
+    for line in lines:
+        k = line.split()
+        l1.append(int(k[0]))
+        l2.append(int(k[1]))
     l1 = sorted(l1)
     l2 = sorted(l2)
-    print(np.sum(np.abs(np.array(l1) - np.array(l2))))
+    s = 0
+    for i in range(len(lines)):
+        s += abs(l1[i] - l2[i])
+    print(s)
 
 def part2(filename): 
     f = open(filename, 'r')
     lines = f.read().splitlines()
-    l1 = [int(line.split()[0]) for line in lines]
-    l2 = [int(line.split()[1]) for line in lines]
+    l1, l2 = [], []
+    for line in lines:
+        k = line.split()
+        l1.append(int(k[0]))
+        l2.append(int(k[1]))
     c = Counter(l2)
     sum = 0
     for n in l1:
@@ -21,5 +30,5 @@ def part2(filename):
     print(sum)
     
     
-# part1('input')
+part1('input')
 part2('input')
